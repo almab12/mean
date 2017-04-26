bce425c23b56fb8065c64ada49457101afe089e1
al <alessandro.marot.bozza@gmail.com>
2017-04-25 21:48:35 -0400

    get Messages

--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,23 @@ var router = express.Router();
 
 var Message = require('../models/message');
 
+router.get('/', function(req, res, next){
+    Message.find()
+        .exec(function(err, messages){
+            if(err){
+                return res.status(500).json({
+                    title: 'an error occured',
+                    error: err
+                });
+            }
+            res.status(201).json({
+                message: 'Succes',
+                obj: messages
+            });
+        });
+});
+
+
 router.post('/', function (req, res, next) {
     var message = new Message({
         content: req.body.content
